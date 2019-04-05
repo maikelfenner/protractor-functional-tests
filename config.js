80e47c0a92342dfb91f74fc273da043161e70329
Maikel Fenner <maikel.jsf@gmail.com>
2019-04-05 18:36:25 -0300

    added jasmine-spec-reporter and test to access index page

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/*.js'],
+  framework: 'jasmine',
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
-   }
- },
-
-  framework: 'jasmine',
+      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
+  },
   onPrepare: function() {
-    var jasmineReporters = require('jasmine-reporters');
-    var junitReporter = new jasmineReporters.JUnitXmlReporter({
-      savePath: '${CIRCLE_TEST_REPORTS}/output/',
-      consolidateAll: true
+    var specReporter = new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailuredSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
     });
-    jasmine.getEnv().addReporter(junitReporter);
+
+    jasmine.getEnv().addReporter(specReporter);
   }
 };
---
+++ specs/test-spec.js
@@ -0,0 +1,9 @@
+describe('demo test', function() {
+	
+    it('access index page', function() {
+        browser.waitForAngularEnabled(false);
+        browser.get('https://www.saucedemo.com/');
+
+        expect(browser.getTitle()).toEqual('Swag Labs');
+	});
+});
